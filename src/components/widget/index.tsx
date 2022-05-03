10b81aceb39741f7fb3ef071a2825e6d4512cdde
Thayrone Paula <thayronedepaula15@gmail.com>
2022-05-02 21:29:18 -0300

    add popOver

--- src/components/widget/index.tsx
+++ src/components/widget/index.tsx
@@ -1,17 +1,20 @@
+import { useState } from 'react'
 import { ChatTeardropDots } from 'phosphor-react'
+import { Popover } from '@headlessui/react'
 
 export function Widget() {
   return (
-    <div className='absolute right-5 bottom-5'>
-
-      <p>Hello world</p>
-      <button className='flex items-center h-12 px-3 text-white rounded-full bg-brand-500 group'>
+    <Popover className='absolute right-5 bottom-5'>
+     <Popover.Panel>Hello world</Popover.Panel>
+      <Popover.Button
+        className='flex items-center h-12 px-3 text-white rounded-full bg-brand-500 group'
+      >
         <ChatTeardropDots className='w-6 h-6' />
         <span className='overflow-hidden transition-all duration-500 ease-linear max-w-0 group-hover:max-w-xs'>
           <span className='pl-2'></span>
           Feedback
         </span>
-      </button>
-    </div>
+      </Popover.Button>
+    </Popover>
   )
 }
